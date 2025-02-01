99d547a2f01f634e345a456f2ee1deeefee1f9a7
Fardowsanuur <Fardowsonuura6@gmail.com>
2025-02-01 11:34:04 -0800

    home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../widgets/app_scaffold.dart';
 import 'package:timeago/timeago.dart' as timeago;
-////////
 
+// Home page where a list of blogs is displayed
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -14,42 +14,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final blogController = Get.put(BlogController());
+  final blogController = Get.put(BlogController()); // Blog controller instance
 
   @override
   void initState() {
     super.initState();
-    blogController.getBlogPost();
+    blogController.getBlogPost(); // Fetch the blog posts when the page loads
   }
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
       appBar: AppBar(
-        title: const Text('Blogs'),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        automaticallyImplyLeading: false, // This removes back arrow
-
+        title: const Text('Blogs'), // Title of the AppBar
+        elevation: 0, // Removes shadow from the AppBar
+        backgroundColor: Colors.white, // AppBar background color
+        foregroundColor: Colors.black, // Text color in the AppBar
+        automaticallyImplyLeading: false, // Removes the back arrow from AppBar
       ),
       body: Obx(() {
+        // Reactive widget updates based on the controller's state
         if (blogController.loading.value) {
-          return const Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator()); // Show loading spinner
         }
 
         if (blogController.data.isEmpty) {
-          return const Center(child: Text('No blogs found'));
+          return const Center(child: Text('No blogs found')); // Show message if no blogs available
         }
 
+        // ListView to display blog posts
         return ListView.builder(
-          padding: const EdgeInsets.all(16),
-          itemCount: blogController.data.length,
+          padding: const EdgeInsets.all(16), // Padding around the list
+          itemCount: blogController.data.length, // Number of blog posts
           itemBuilder: (context, index) {
-            final blog = blogController.data[index];
+            final blog = blogController.data[index]; // Get individual blog data
             return BlogItem(
-              blog: blog,
-              onTap: () => Get.to(() => BlogDetailsPage(blog: blog)),
+              blog: blog, 
+              onTap: () => Get.to(() => BlogDetailsPage(blog: blog)), // Navigate to blog details page on tap
             );
           },
         );
@@ -58,9 +59,10 @@ class _HomeState extends State<Home> {
   }
 }
 
+// Widget to display each blog item in the list
 class BlogItem extends StatelessWidget {
   final Blog blog;
-  final VoidCallback onTap;
+  final VoidCallback onTap; // Callback function when tapped
 
   const BlogItem({
     Key? key,
@@ -70,64 +72,63 @@ class BlogItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final blogController = Get.find<BlogController>();
+    final blogController = Get.find<BlogController>(); // Get the blog controller instance
 
     return Card(
-      margin: const EdgeInsets.only(bottom: 16),
+      margin: const EdgeInsets.only(bottom: 16), // Card margin between items
       child: InkWell(
-        onTap: onTap,
+        onTap: onTap, // Trigger onTap when the card is tapped
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             if (blog.image.isNotEmpty)
               Image.network(
-                blog.image,
+                blog.image, // Display the blog image
                 height: 200,
                 width: double.infinity,
-                fit: BoxFit.cover,
+                fit: BoxFit.cover, // Cover the space with the image
                 errorBuilder: (context, error, stackTrace) => Container(
-                  height: 200,
+                  height: 200, // Placeholder in case of error
                   color: Colors.grey[300],
-                  child: const Icon(Icons.error),
+                  child: const Icon(Icons.error), // Display error icon if image fails to load
                 ),
               ),
             Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(16), // Padding for the blog content
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween, // Align title and bookmark icon
                     children: [
                       Expanded(
                         child: Text(
-                          blog.title,
+                          blog.title, // Display the blog title
                           style: Theme.of(context).textTheme.titleLarge,
                         ),
                       ),
                       Obx(() => IconButton(
                         icon: Icon(
-                              blogController.isBookmarked(blog)
-                                  ? Icons.bookmark
-                                  : Icons.bookmark_border,
-                              color: blogController.isBookmarked(blog)
-                                  ? Colors.blue
-                                  : Colors.grey,
+                          blogController.isBookmarked(blog)
+                              ? Icons.bookmark // Display filled bookmark icon
+                              : Icons.bookmark_border, // Display empty bookmark icon
+                          color: blogController.isBookmarked(blog)
+                              ? Colors.blue // Color when bookmarked
+                              : Colors.grey, // Color when not bookmarked
                         ),
-                            onPressed: () =>
-                                blogController.toggleBookmark(blog),
-                          )),
+                        onPressed: () =>
+                            blogController.toggleBookmark(blog), // Toggle bookmark status
+                      )),
                     ],
                   ),
-
                   const SizedBox(height: 8),
                   Text(
-                    'Posted by ${blog.user.email}',
+                    'Posted by ${blog.user.email}', // Display user email who posted the blog
                     style: Theme.of(context).textTheme.bodySmall,
                   ),
                   const SizedBox(height: 4),
                   Text(
-                    timeago.format(DateTime.parse(blog.createdAt)),
+                    timeago.format(DateTime.parse(blog.createdAt)), // Format the creation date using timeago
                     style: Theme.of(context).textTheme.bodySmall,
                   ),
                 ],
@@ -140,6 +141,7 @@ class BlogItem extends StatelessWidget {
   }
 }
 
+// Blog details page that shows full content of a selected blog
 class BlogDetailsPage extends StatelessWidget {
   final Blog blog;
 
@@ -149,7 +151,7 @@ class BlogDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(blog.title),
+        title: Text(blog.title), // Display the blog title on the AppBar
         elevation: 0,
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
@@ -160,13 +162,13 @@ class BlogDetailsPage extends StatelessWidget {
           children: [
             if (blog.image.isNotEmpty)
               Image.network(
-                blog.image,
+                blog.image, // Display the blog image
                 height: 300,
                 width: double.infinity,
                 fit: BoxFit.cover,
                 errorBuilder: (context, error, stackTrace) => Container(
                   height: 300,
-                  color: Colors.grey[300],
+                  color: Colors.grey[300], // Placeholder for image loading error
                   child: const Icon(Icons.error),
                 ),
               ),
@@ -176,7 +178,7 @@ class BlogDetailsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    blog.title,
+                    blog.title, // Display the blog title
                     style: Theme.of(context).textTheme.headlineSmall,
                   ),
                   const SizedBox(height: 8),
@@ -184,17 +186,17 @@ class BlogDetailsPage extends StatelessWidget {
                     children: [
                       const Icon(Icons.person, size: 16),
                       const SizedBox(width: 4),
-                      Text(blog.user.email),
+                      Text(blog.user.email), // Display user email
                       const Spacer(),
                       Text(
-                        timeago.format(DateTime.parse(blog.createdAt)),
+                        timeago.format(DateTime.parse(blog.createdAt)), // Format and display the creation date
                         style: Theme.of(context).textTheme.bodySmall,
                       ),
                     ],
                   ),
                   const SizedBox(height: 16),
                   Text(
-                    blog.story,
+                    blog.story, // Display the full blog content
                     style: Theme.of(context).textTheme.bodyLarge,
                   ),
                 ],
