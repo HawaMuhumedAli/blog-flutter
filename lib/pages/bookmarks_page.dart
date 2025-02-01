2f9ae6ea14ffc6417b52dbddb434220e58e59d80
Fardowsanuur <Fardowsonuura6@gmail.com>
2025-02-01 11:00:53 -0800

    bookmarks_page.dart

--- lib/pages/bookmarks_page.dart
+++ lib/pages/bookmarks_page.dart
@@ -4,27 +4,30 @@ import '../controllers/blogController.dart';
 import '../widgets/app_scaffold.dart';
 import 'home.dart';
 
+// BookmarksPage is a stateless widget that displays bookmarked blog posts.
 class BookmarksPage extends GetView<BlogController> {
   const BookmarksPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      initialIndex: 1,
+      initialIndex: 1, // Sets the bottom navigation bar index to Bookmarks
       appBar: AppBar(
         title: const Text('Bookmarks'),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        automaticallyImplyLeading: false, // This removes back arrow
+        elevation: 0, // Removes the shadow from the AppBar
+        backgroundColor: Colors.white, // Sets AppBar background color to white
+        foregroundColor: Colors.black, // Sets AppBar text color to black
+        automaticallyImplyLeading: false, // This removes the back arrow
       ),
       body: Obx(() {
+        // If there are no bookmarked posts, display a message
         if (controller.bookmarkedPosts.isEmpty) {
           return const Center(
             child: Text('No bookmarked posts yet'),
           );
         }
 
+        // Displays a list of bookmarked blog posts
         return ListView.builder(
           padding: const EdgeInsets.all(16),
           itemCount: controller.bookmarkedPosts.length,
@@ -33,6 +36,7 @@ class BookmarksPage extends GetView<BlogController> {
             return BlogItem(
               blog: blog,
               onTap: () => Get.toNamed('/blog-details', arguments: blog),
+              // Navigates to blog details page when tapped
             );
           },
         );
