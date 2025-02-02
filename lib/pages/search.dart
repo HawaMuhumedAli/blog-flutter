522979c9ca3ee40b625d8bf46ea4276a43102a37
Fardowsanuur <Fardowsonuura6@gmail.com>
2025-02-01 18:35:52 -0800

    search.dart

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 import '../widgets/app_scaffold.dart';
 
+/// A stateless widget representing the Search Page.
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      initialIndex: 3,
-      appBar: AppBar(
-        automaticallyImplyLeading: false, // This removes back arrow
+      initialIndex: 3, // Sets the initial selected index for navigation
 
-        title: const Text('Search'),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Removes the default back arrow
+        title: const Text('Search'), // Sets the title of the AppBar
+        elevation: 0, // Removes AppBar shadow for a flat design
+        backgroundColor: Colors.white, // Sets the background color of AppBar
+        foregroundColor: Colors.black, // Sets the color of title and icons
       ),
+
       body: const Center(
-        child: Text('Search Page'),
+        child: Text('Search Page'), // Placeholder text for the search page
       ),
     );
   }
